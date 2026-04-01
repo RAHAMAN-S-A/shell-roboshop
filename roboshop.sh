f00bb73a16a8412a54dfd94c0552f8b5353a80be
rahaman <you@example.com>
2026-04-01 13:29:02 +0530

    FIRST COMMIT

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 present=$(pwd)
+LOGS_FOLDER="/var/logs/roboshop/"
+LOGS_FILE="/var/logs/roboshop/.$0.logs"
 
 user_id=$(id -u)
 
@@ -11,6 +13,28 @@ if [ $user_id -ne 0 ]; then
   echo "use root user"
   exit 1
   echo " sudo user "
+mkdir -p $LOGS_FOLDER
 
 fi
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
 
+valadiate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE"
+        exit 1
+    else
+        echo "$2 ... SUCCESS"
+    fi
+}
+
+
+dnf install nginx -y
+valadaiate $? " installing nginx "
+
+dnf install mysql -y
+VALIDATE $? "Installing Mysql"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
