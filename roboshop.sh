955f608a46f1f5e88eb77c56cf2e5e23f168bb88
rahaman <you@example.com>
2026-04-01 14:58:00 +0530

    FIRST COMMIT

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 present=$(pwd)
-LOGS_FOLDER="/var/logs/roboshop/"
+LOGS_FOLDER="var/logs/roboshop/"
 LOGS_FILE="/var/logs/roboshop/.$0.logs"
+sg_id=sg-00b80ff8ce8c7583c
+ami_Id=ami-0220d79f3f480ecf5
 
 user_id=$(id -u)
 
@@ -13,7 +15,7 @@ if [ $user_id -ne 0 ]; then
   echo "use root user"
   exit 1
   echo " sudo user "
-  
+
 mkdir -p $LOGS_FOLDER
 
 fi
@@ -42,3 +44,19 @@ sleep=10
 
 dnf install nodejs -y
 VALIDATE $? "Installing nodejs" | tee -a $LOGS_FILE
+
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+        #VALIDATE $? "$package installation"
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
+    fi
+done
+
+
+aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
